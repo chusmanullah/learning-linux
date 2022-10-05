60972eebed303d008aaa1c47bd7b8c27f51fa4bb
Usman Asif <chusmanullah@gmail.com>
2022-10-05 14:54:38 -0400

    update

---
+++ scripts/first.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this is how you add comments in linux shell script
+#there is no way to add multiline comment in shell script
+# we can only add single line comments in shell script
+
+ls | > file.txt
+
