1d53b557f4d04006e70f9d4cb089698f6a4458d0
Usman Asif <chusmanullah@gmail.com>
2022-10-06 04:29:32 -0400

    update

--- scripts/first.sh
+++ scripts/first.sh
@@ -4,5 +4,26 @@
 #there is no way to add multiline comment in shell script
 # we can only add single line comments in shell script
 
-ls | > file.txt
+# which bash will give the location of where the bash is located
+
+sleep 1
+# to display you use echo command
+echo "hello world Don't forget to be awesome"
+
+# what are the variables
+# two types of variables System and userdefined variables
+# system variables are usually defined capital by convention
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+#For user defined variables
+name=Mark
+echo The name is $name
+#the variable name should not be start as number
+# how to take input from user
+echo "Enter name: "
+read name
+echo "Entered name : $name"
 
